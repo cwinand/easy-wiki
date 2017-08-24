aa0868d382768e259bd395b826fce0a64e188855
Winand, Christopher <cwinand.6@gmail.com>
2017-08-24 14:48:55 -0400

    refactor to improve readability

--- src/reducers/categories.js
+++ src/reducers/categories.js
@@ -1,46 +1,47 @@
 import * as types from '../constants/action_types';
 
-const categories = (
-  state = {
-    isFetching: false,
-    items: []
-  }, action ) => {
-    switch ( action.type ) {
-      case types.CATEGORIES_REQUEST:
-        return {
-          ...state,
-          isFetching: true
-        }
-      case types.GET_CATEGORIES_SUCCESS:
-        return {
-          isFetching: false,
-          items: action.data
-        }
-      case types.POST_CATEGORY_SUCCESS:
-        return {
-          isFetching: false,
-          items: [
-            ...state.items,
-            action.data
-          ]}
-      case types.DELETE_CATEGORY_SUCCESS:
-        return {
-          isFetching: false,
-          items: state.items.filter( ( item ) => item.id !== action.id )
-        }
-      case types.PUT_CATEGORY_SUCCESS:
-        return {
-          isFetching: false,
-          items: state.items.map( ( item ) => item.id === action.data.id ? action.data : item )
-        }
-      case types.PUT_CATEGORIES_SUCCESS:
-        return {
-          isFetching: false,
-          items: state.items.map( ( item ) => action.data.hasOwnProperty( item.id ) ? action.data[ item.id ] : item )
-        }
-      default:
-        return state;
-    }
+const initialState = {
+  isFetching: false,
+  items: []
+}
+
+const categories = ( state = initialState, action ) => {
+  switch ( action.type ) {
+    case types.CATEGORIES_REQUEST:
+      return {
+        ...state,
+        isFetching: true
+      }
+    case types.GET_CATEGORIES_SUCCESS:
+      return {
+        isFetching: false,
+        items: action.data
+      }
+    case types.POST_CATEGORY_SUCCESS:
+      return {
+        isFetching: false,
+        items: [
+          ...state.items,
+          action.data
+        ]}
+    case types.DELETE_CATEGORY_SUCCESS:
+      return {
+        isFetching: false,
+        items: state.items.filter( ( item ) => item.id !== action.id )
+      }
+    case types.PUT_CATEGORY_SUCCESS:
+      return {
+        isFetching: false,
+        items: state.items.map( ( item ) => item.id === action.data.id ? action.data : item )
+      }
+    case types.PUT_CATEGORIES_SUCCESS:
+      return {
+        isFetching: false,
+        items: state.items.map( ( item ) => action.data.hasOwnProperty( item.id ) ? action.data[ item.id ] : item )
+      }
+    default:
+      return state;
+  }
 }
 
 export default categories;
