b726cb8bda4edeca7afa5b1d4dd5b4b16ec44db0
Winand, Christopher <cwinand.6@gmail.com>
2017-08-30 16:50:15 -0400

    Add redux-logger to dev environment, move redux-mock-store to dev

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 
 import './styles/styles.scss';
 import 'font-awesome/scss/font-awesome.scss';
@@ -12,7 +13,7 @@ import { apiGetCategories } from './actions/categories';
 import App from './components/App';
 import reducers from './reducers';
 
-let store = createStore( reducers, applyMiddleware( thunkMiddleware ) );
+let store = createStore( reducers, applyMiddleware( thunkMiddleware, logger ) );
 
 store.dispatch( apiGetCategories() )
 
