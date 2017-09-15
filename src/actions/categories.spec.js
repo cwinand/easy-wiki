cb8233445931b669f1fd3e7b465871865655ff7e
Winand, Christopher <cwinand.6@gmail.com>
2017-09-15 14:46:40 -0400

    Fix categories action specs

--- src/actions/categories.spec.js
+++ src/actions/categories.spec.js
@@ -9,6 +9,9 @@ axios.defaults.adapter = httpAdapter;
 import configureMockStore from 'redux-mock-store';
 import thunkMiddleware from 'redux-thunk';
 
+import { normalize } from 'normalizr'
+import { categoryEntity, categoriesSchema } from './schema'
+
 const mockStore = configureMockStore( [ thunkMiddleware ] )
 
 describe( 'Synchronous category actions', () => {
@@ -24,33 +27,62 @@ describe( 'Synchronous category actions', () => {
 
   test( 'should create an action to show/hide the add category form', () => {
     const expectedShowAction = {
-      type: types.CHANGE_FORM_VISIBILITY,
+      type: types.CHANGE_NEW_VISIBILITY,
       status: true
     }
     const expectedHideAction = {
-      type: types.CHANGE_FORM_VISIBILITY,
+      type: types.CHANGE_NEW_VISIBILITY,
       status: false
     }
 
-    expect( actions.changeFormVisibility( true ) ).toEqual( expectedShowAction )
-    expect( actions.changeFormVisibility( false ) ).toEqual( expectedHideAction )
+    expect( actions.changeNewVisibility( true ) ).toEqual( expectedShowAction )
+    expect( actions.changeNewVisibility( false ) ).toEqual( expectedHideAction )
   })
 
-  test( 'should create an action to indicate a categories API request is ocurring', () => {
-    const expectedAction = {
-      type: types.CATEGORIES_REQUEST
+  test( 'should create an action to show/hide the edit category form', () => {
+    const expectedShowAction = {
+      type: types.CHANGE_EDIT_VISIBILITY,
+      status: true
+    }
+    const expectedHideAction = {
+      type: types.CHANGE_EDIT_VISIBILITY,
+      status: false
     }
 
-    expect( actions.categoriesRequest() ).toEqual( expectedAction );
-  });
+    expect( actions.changeEditVisibility( true ) ).toEqual( expectedShowAction )
+    expect( actions.changeEditVisibility( false ) ).toEqual( expectedHideAction )
+  })
+
+  test( 'should create an action to indicate the status of a categories API request', () => {
+    const expectedFetchingAction = {
+      type: types.CATEGORIES_REQUEST_STATUS,
+      status: true
+    }
+    const expectedFinishedAction = {
+      type: types.CATEGORIES_REQUEST_STATUS,
+      status: false
+    }
+
+    expect( actions.categoriesFetching( true ) ).toEqual( expectedFetchingAction );
+    expect( actions.categoriesFetching( false ) ).toEqual( expectedFinishedAction );
+  })
+
+  test( 'should create an action to move a category in the array of categories', () => {
+    const expectedAction = {
+      type: types.MOVE_CATEGORY,
+      data: {}
+    }
+
+    expect( actions.moveCategory( {} ) ).toEqual( expectedAction )
+  })
 
   test( 'should create an action to receive multiple categories', () => {
     const expectedAction = {
       type: types.GET_CATEGORIES_SUCCESS,
-      data: [ {}, {}, {} ]
+      data: normalize( [ { id: 1 }, { id: 2 } ], categoriesSchema )
     }
 
-    expect( actions.getCategoriesSuccess( [ {}, {}, {} ] ) ).toEqual( expectedAction );
+    expect( actions.getCategoriesSuccess( [ { id: 1 }, { id: 2 } ] ) ).toEqual( expectedAction );
   })
 
   // Add test to handle failure of get request
@@ -58,7 +90,7 @@ describe( 'Synchronous category actions', () => {
   test( 'should create an action to receive a single category', () => {
     const expectedAction = {
       type: types.POST_CATEGORY_SUCCESS,
-      data: { id: 1 }
+      data: normalize( { id: 1 }, categoryEntity )
     }
 
     expect( actions.postCategorySuccess( { id: 1 } ) ).toEqual( expectedAction );
@@ -69,7 +101,7 @@ describe( 'Synchronous category actions', () => {
   test( 'should create an action to update a category', () => {
     const expectedAction = {
       type: types.PUT_CATEGORY_SUCCESS,
-      data: { id: 1 }
+      data: normalize( { id: 1 }, categoryEntity )
     }
 
     expect( actions.putCategorySuccess( { id: 1 } ) ).toEqual( expectedAction );
@@ -92,13 +124,23 @@ describe( 'Asynchronous category functions', () => {
 
   test( 'creates GET_CATEGORIES_SUCCESS when request has succeeded', () => {
     const expectedActions = [
-      { type: types.CATEGORIES_REQUEST },
-      { type: types.GET_CATEGORIES_SUCCESS, data: [ { id: 1 } ] }
+      {
+        type: types.CATEGORIES_REQUEST_STATUS, 
+        status: true
+      },
+      {
+        type: types.CATEGORIES_REQUEST_STATUS, 
+        status: false
+      },
+      {
+        type: types.GET_CATEGORIES_SUCCESS,
+        data: normalize( [ { id: 1 }, { id: 2 } ], categoriesSchema )
+      }
     ]
 
     nock( 'http://localhost:3002' )
       .get( '/api/categories' )
-      .reply( 200, [ { id: 1 } ] )
+      .reply( 200, [ { id: 1 }, { id: 2 } ] )
 
     const store = mockStore( { items: [] } )
 
@@ -109,8 +151,18 @@ describe( 'Asynchronous category functions', () => {
 
   test( 'creates POST_CATEGORY_SUCCESS when request has succeeded', () => {
     const expectedActions = [
-      { type: types.CATEGORIES_REQUEST },
-      { type: types.POST_CATEGORY_SUCCESS, data: { id: 1, title: "Test" } }
+      {
+        type: types.CATEGORIES_REQUEST_STATUS, 
+        status: true
+      },
+      {
+        type: types.CATEGORIES_REQUEST_STATUS, 
+        status: false
+      },
+      {
+        type: types.POST_CATEGORY_SUCCESS,
+        data: normalize( { id: 1, title: "Test" }, categoryEntity )
+      }
     ]
 
     nock( 'http://localhost:3002' )
@@ -126,8 +178,18 @@ describe( 'Asynchronous category functions', () => {
 
   test( 'creates PUT_CATEGORY_SUCCESS when request has succeeded', () => {
     const expectedActions = [
-      { type: types.CATEGORIES_REQUEST },
-      { type: types.PUT_CATEGORY_SUCCESS, data: { id: 1, title: "Test" } }
+      {
+        type: types.CATEGORIES_REQUEST_STATUS, 
+        status: true
+      },
+      {
+        type: types.CATEGORIES_REQUEST_STATUS, 
+        status: false
+      },
+      {
+        type: types.PUT_CATEGORY_SUCCESS,
+        data: normalize( { id: 1, title: "Test" }, categoryEntity )
+      }
     ]
 
     nock( 'http://localhost:3002' )
@@ -143,8 +205,18 @@ describe( 'Asynchronous category functions', () => {
 
   test( 'creates PUT_CATEGORIES_SUCCESS when request has succeeded', () => {
     const expectedActions = [
-      { type: types.CATEGORIES_REQUEST },
-      { type: types.PUT_CATEGORIES_SUCCESS, data: { 1: { id: 1, order: 1 } } }
+      {
+        type: types.CATEGORIES_REQUEST_STATUS, 
+        status: true
+      },
+      {
+        type: types.CATEGORIES_REQUEST_STATUS, 
+        status: false
+      },
+      {
+        type: types.PUT_CATEGORIES_SUCCESS,
+        data: normalize( [ { id: 1, order: 1 } ], categoriesSchema )
+      }
     ]
 
     nock( 'http://localhost:3002' )
@@ -160,8 +232,18 @@ describe( 'Asynchronous category functions', () => {
 
   test( 'creates DELETE_CATEGORY_SUCCESS when request has succeeded', () => {
     const expectedActions = [
-      { type: types.CATEGORIES_REQUEST },
-      { type: types.DELETE_CATEGORY_SUCCESS, id: 1 }
+      {
+        type: types.CATEGORIES_REQUEST_STATUS, 
+        status: true
+      },
+      {
+        type: types.CATEGORIES_REQUEST_STATUS, 
+        status: false
+      },
+      {
+        type: types.DELETE_CATEGORY_SUCCESS,
+        id: 1
+      }
     ]
 
     nock( 'http://localhost:3002' )
