fcda351a6cc0c16e27676595e98c243efe7859fe
Winand, Christopher <cwinand.6@gmail.com>
2017-09-15 16:48:06 -0400

    Rewrite all categories reducer specs

--- src/reducers/categories.spec.js
+++ src/reducers/categories.spec.js
@@ -1,190 +1,176 @@
-import categories from './categories';
-import * as types from '../constants/categories_types';
+import categories from './categories'
+import * as types from '../constants/categories_types'
 
-describe( 'Categories reducer', () => {
-  test( 'sets isFormShown to true', () => {
+import { normalize } from 'normalizr'
+import { categoriesSchema, categoryEntity } from '../actions/schema'
+
+describe( 'Categories Reducer', () => {
+  test( 'adds multiple categories', () => {
+    const action = {
+      type: types.GET_CATEGORIES_SUCCESS,
+      data: normalize( [ { id: 1 }, { id: 2 } ], categoriesSchema )
+    }
+
+    const state = categories( {}, action )
+
+    expect( state.byId ).toEqual( { 1: { id: 1 }, 2: { id: 2 } } )
+    expect( state.allIds ).toEqual( [ 1, 2 ] )
+  })
+
+  test( 'adds a single category', () => {
+    const action = {
+      type: types.POST_CATEGORY_SUCCESS,
+      data: normalize( { id: 1 }, categoryEntity )
+    }
+
+    const state = categories( {}, action )
+
+    expect( state.byId ).toEqual( { 1: { id: 1 } } )
+    expect( state.allIds ).toEqual( [ 1 ] )
+  })
+
+  test( 'updates a single category', () => {
+    const action = {
+      type: types.PUT_CATEGORY_SUCCESS,
+      data: normalize( { id: 1, title: "Changed" }, categoryEntity )
+    }
+    const oldState = {
+      byId: {
+        1: {
+          id: 1,
+          title: "Not changed"
+        }
+      }
+    }
+    const state = categories( oldState, action )
+
+    expect( state.byId ).toEqual( { 1: { id: 1, title: "Changed" } } )
+  })
+
+  test( 'removes a single category', () => {
+    const action = {
+      type: types.DELETE_CATEGORY_SUCCESS,
+      id: 1
+    }
+    const oldState = {
+      byId: {
+        1: {
+          id: 1,
+        }
+      },
+      allIds: [ 1 ]
+    }
+    const state = categories( oldState, action )
+
+    expect( state.byId ).toEqual( {} )
+    expect( state.allIds ).toEqual( [] )
+  })
+
+  test( 'updates multiple categories', () => {
+    const action = {
+      type: types.PUT_CATEGORIES_SUCCESS,
+      data: normalize( [ { id: 2, order: 0 }, { id: 1, order: 1 } ], categoriesSchema )
+    }
+    const oldState = {
+      byId: {
+        1: {
+          id: 1,
+          order: 0
+        },
+        2: {
+          id: 2,
+          order: 1
+        }
+      }
+    }
+    const state = categories( oldState, action )
+
+    expect( state.byId ).toEqual( { 1: { id: 1, order: 1 }, 2: { id: 2, order: 0 } } )
+  })
+
+  test( 'moves a category in the allIds array', () => {
+    const action = {
+      type: types.MOVE_CATEGORY,
+      data: {
+        categoryIds: [ 1, 2 ],
+        oldIndex: 1,
+        newIndex: 0
+      }
+    }
+    const oldState = {
+      allIds: [ 1, 2 ]
+    }
+    const state = categories( oldState, action )
+
+    expect( state.allIds ).toEqual( [ 2, 1 ])
+  })
+
+  test( 'resets allIds if save failed', () => {
+    const action = {
+      type: types.PUT_CATEGORIES_FAILURE,
+      previousCategoryIds: [ 1, 2 ]
+    }
+    const oldState = {
+      allIds: [ 2, 1 ]
+    }
+    const state = categories( oldState, action )
+
+    expect( state.allIds ).toEqual( [ 1, 2 ] )
+  })
+
+  test( 'sets id of selectedCategory', () => {
+    const action = {
+      type: types.SELECT_CATEGORY,
+      id: 1
+    }
+    const oldState = {
+      selectedCategory: {
+        id: null
+      }
+    }
+    const state = categories( oldState, action )
+
+    expect( state.selectedCategory.id ).toEqual( 1 )
+  })
+
+  test( 'sets status of selected category edit form visibility', () => {
+    const action = {
+      type: types.CHANGE_EDIT_VISIBILITY,
+      status: true
+    }
+    const oldState = {
+      selectedCategory: {
+        isFormShown: false
+      }
+    }
+    const state = categories( oldState, action )
+
+    expect( state.selectedCategory.isFormShown ).toBe( true )
+  })
+
+  test( 'sets status of new category form visibility', () => {
+    const action = {
+      type: types.CHANGE_NEW_VISIBILITY,
+      status: true
+    }
     const oldState = {
       isFormShown: false
     }
-    const newState = categories( oldState, {
-      type: types.CHANGE_FORM_VISIBILITY,
+    const state = categories( oldState, action )
+
+    expect( state.isFormShown ).toBe( true )
+  })
+
+  test( 'sets status of category isFetching flag', () => {
+    const action = {
+      type: types.CATEGORIES_REQUEST_STATUS,
       status: true
-    })
+    }
+    const oldState = {
+      isFetching: false
+    }
+    const state = categories( oldState, action )
 
-    expect( newState.isFormShown ).toBe( true )
+    expect( state.isFetching ).toBe( true )
   })
-
-  test( 'sets isFormShown to false', () => {
-    const oldState = {
-      isFormShown: true
-    }
-    const newState = categories( oldState, {
-      type: types.CHANGE_FORM_VISIBILITY,
-      status: false
-    })
-
-    expect( newState.isFormShown ).toBe( false )
-  })
-
-  test( 'sets selected to the id of the selected category', () => {
-    const oldState = {
-      selected: undefined
-    }
-    const newState = categories( oldState, {
-      type: types.SELECT_CATEGORY,
-      id: 1
-    })
-
-    expect( newState.selected ).toBe( 1 )
-  })
-
-  test( 'sets isFetching to true', () => {
-    const oldState = {
-      isFetching: false,
-      items: []
-    }
-    const newState = categories( oldState, { type: types.CATEGORIES_REQUEST } )
-
-    expect( newState.isFetching ).toBe( true );
-  });
-
-  test( 'adds multiple categories to state', () => {
-    const oldState = {
-      isFetching: true,
-      items: []
-    }
-    const newState = categories( oldState, {
-      type: types.GET_CATEGORIES_SUCCESS,
-      data: [ {}, {}, {} ]
-    });
-
-    expect( newState.items.length ).toBe( 3 );
-  });
-
-  test( 'adds single category to state', () => {
-    const oldState = {
-      isFetching: true,
-      items: []
-    };
-    const newState = categories( oldState, {
-      type: types.POST_CATEGORY_SUCCESS,
-      data: {
-        title: 'Test Category',
-        order: 0,
-        id: 1
-      }
-    });
-
-    expect( newState.isFetching ).toBe( false );
-    expect( newState.items ).toEqual([
-      {
-        title: 'Test Category',
-        order: 0,
-        id: 1
-      }
-    ]);
-  });
-
-  test( 'removes single category from state', () => {
-    const oldState = {
-      isFetching: true,
-      items: [
-        {
-          title: 'Test Category',
-          order: 0,
-          id: 1
-        }
-      ]
-    };
-    const newState = categories( oldState, {
-      type: types.DELETE_CATEGORY_SUCCESS,
-      id: 1
-    });
-
-    expect( newState.isFetching ).toBe( false );
-    expect( newState.items ).toEqual( [] );
-  });
-
-  test( 'updates single category in state', () => {
-    const oldState = {
-      isFetching: true,
-      items: [
-        {
-          title: 'Test Category',
-          order: 0,
-          id: 1
-        }
-      ]
-    }
-    const newState = categories( oldState, {
-      type: types.PUT_CATEGORY_SUCCESS,
-      data: {
-        title: 'Test Category Changed',
-        order: 1,
-        id: 1
-      }
-    });
-
-    expect( newState.isFetching ).toBe( false );
-    expect( newState.items[ 0 ] ).toEqual({
-      title: 'Test Category Changed',
-      order: 1,
-      id: 1
-    });
-  });
-
-  test( 'updates multiple categories in state', () => {
-    const oldState = {
-      isFetching: true,
-      items: [
-        {
-          title: 'Test Category 1',
-          id: 1,
-          order: 0
-        },
-        {
-          title: 'Test Category 2',
-          id: 2,
-          order: 1
-        },
-        {
-          title: 'Test Category 3',
-          id: 3,
-          order: 2
-        }
-      ]
-    };
-    const newState = categories( oldState, {
-      type: types.PUT_CATEGORIES_SUCCESS,
-      data: {
-        1: {
-          title: 'Test Category 1',
-          id: 1,
-          order: 1
-        },
-        2: {
-          title: 'Test Category 2',
-          id: 2,
-          order: 0
-        }
-      }
-    });
-
-    expect( newState.isFetching ).toBe( false );
-    expect( newState.items[ 0 ] ).toEqual({
-      title: 'Test Category 1',
-      id: 1,
-      order: 1
-    });
-    expect( newState.items[ 1 ] ).toEqual({
-      title: 'Test Category 2',
-      id: 2,
-      order: 0
-    });
-    expect( newState.items[ 2 ] ).toEqual({
-      title: 'Test Category 3',
-      id: 3,
-      order: 2
-    });
-  });
-});
+})
